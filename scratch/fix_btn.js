40744ce07913141a56b160ad64d37353f3d67da4
Murillooh <muurisattos@gmail.com>
2026-06-07 23:46:54 -0300

    chore: atualiza .gitignore e remove arquivos temporarios do tracking

--- scratch/fix_btn.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('public/index.html', 'utf8');
-c = c.replace('btn.onclick = showAppConfigModal;', 'btn.onclick = () => { setActiveDay("config"); };');
-fs.writeFileSync('public/index.html', c);
-console.log('Button fixed!');
