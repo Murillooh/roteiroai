dbbb8b77e9ba9640bde77e210ea51c6fc86c64f8
Murillooh <muurisattos@gmail.com>
2026-06-07 23:42:28 -0300

    chore: remove arquivos temporarios de debug do repositorio

--- fix.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const files = ['android/app/src/main/assets/public/index.html', 'public/index.html']; files.forEach(file => { let html = fs.readFileSync(file, 'utf8'); html = html.split(zIndex = '-9999').join(zIndex = '1'); html = html.split(z-index', '-9999').join(z-index', '1'); fs.writeFileSync(file, html); console.log('Updated ' + file); });
